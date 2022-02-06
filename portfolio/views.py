23585f0b1a8c1e4f538770f50c231d8bcc023c51
Huseyn Mammadov <huseynmv@yahoo.com>
2022-02-06 17:03:03 +0400

    experience added

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,9 +6,12 @@ def main(request):
     about = About.objects.all()
     testimonial = Testimonials.objects.all()
     education = Education.objects.all()
+    experience = Experience.objects.all()
+    
     context = {
         'about':about,
         'testimonial':testimonial,
-        'education':education
+        'education':education,
+        'experience':experience
     }
     return render(request, 'index.html', context)
